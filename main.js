2f012a1977aaba08316dbe302db9240e7195aa3a
やきぱな <78954776+yakipana@users.noreply.github.com>
2022-04-23 01:31:10 +0900

    作成

---
+++ main.js
@@ -0,0 +1,48 @@
+var hyoujihihyouji = 0;
+
+function syoki(){
+    document.getElementById("menu").style.visibility ="hidden";
+	document.addEventListener("mousewheel", notscroll, { passive: false });
+	document.addEventListener("touchmove", notscroll, { passive: false });
+    hyoujihihyouji = 0;
+};
+
+function notscroll(e) {
+	e.preventDefault();
+}
+
+function yyyymmddhhmiss() {
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var mm = toDoubleDigits(date.getMonth() + 1);
+    var dd = toDoubleDigits(date.getDate());
+    var hh = toDoubleDigits(date.getHours());
+    var mi = toDoubleDigits(date.getMinutes());
+    var ss = toDoubleDigits(date.getSeconds());
+    var youbi = date.getDay();
+    var youbirisuto = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][youbi];
+    var element = document.getElementById("tokei");
+    element.innerHTML = hh + ':' + mi;
+    var element = document.getElementById("hizuke");
+    element.innerHTML = yyyy + '/' + mm + '/' + dd + " " + youbirisuto;
+}
+  var toDoubleDigits = function(num) {
+    num += "";
+    if (num.length === 1) {
+      num = "0" + num;
+    }
+   return num;
+  };
+  setInterval(yyyymmddhhmiss, 100);
+
+function menuopen(){
+    if(hyoujihihyouji == 0){
+        document.getElementById("menu").style.visibility ="visible";
+        document.getElementById("topbartxt").textContent = "×";
+        hyoujihihyouji = 1;
+    }else{
+        document.getElementById("menu").style.visibility ="hidden";
+        document.getElementById("topbartxt").textContent = "＝";
+        hyoujihihyouji = 0;
+    }
+};
